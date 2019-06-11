977e0c3d1ba30fe6c5345d2e5ba3bcfd3e9c4a47
Michael Huang <50892089+MichaelHuangXiaifei@users.noreply.github.com>
2019-06-11 15:50:57 +0800

    Add files via upload

--- day11_Exercises.py
+++ day11_Exercises.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Time:
     """Represent a time object. Attributes: hour, minute, second"""
 
@@ -10,10 +13,82 @@ class Time:
         return "%d:%d:%d" % (self.hour, self.minute, self.second)
 
 
+class Card:
+    """Represents a standard playing card."""
+
+    suit_names = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+    rank_names = ['None', 'Ace', '2', '3', '4', '5',
+                  '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+
+    def __lt__(self, other):
+        # check the suits
+        if self.suit < other.suit:
+            return True
+        if self.suit > other.suit:
+            return False
+        # suits are the same... check ranks
+        return self.rank < other.rank
+
+    def __str__(self):
+        return "%s of %s" % (Card.rank_names[self.rank], Card.suit_names[self.suit])
+
+    def __init__(self, suit=0, rank=0):
+        self.suit = suit
+        self.rank = rank
+
+
+class Deck:
+    """Represents a set of cards"""
+
+    def pop_card_from_top(self):
+        return self.cards.pop(0)
+
+    def random_pop_card(self):
+        return self.cards.pop(random.randint(0, len(self.cards) - 1))
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def sort(self):
+        self.cards.sort()
+
+    def __str__(self):
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return "\n".join(res)
+
+    def __init__(self):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                card = Card(suit, rank)
+                self.cards.append(card)
+
+
+class Hand(Deck):
+    """Represents a hand of playing cards"""
+
+    def __str__(self):
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return "\n".join(res)
+
+    def __init__(self, label=''):
+        self.cards = []
+        self.label = label
+
+
 time0 = Time(25, 30, 2)
+deck = Deck()
+deck.shuffle()
+
+
 # 11.1
-
-
 def mul_time(time, number):
     total_second = time.hour * 3600 + time.minute * 60 + time.second
     total_second *= number
@@ -37,3 +112,17 @@ def average_pace(finishing_time, distance):
 
 
 print(average_pace(time0, 2))
+
+
+# 11.3
+def deal_hands(number_of_hands, number_of_cards_per_hand):
+    hands = []
+    for hand in range(number_of_hands):
+        hand = Hand('hand%d' % hand)
+        for i in range(number_of_cards_per_hand):
+            hand.add_card(deck.pop_card_from_top())
+        hands.append(hand)
+    return hands
+
+
+print(deal_hands(2, 2))
---
+++ project4.py
@@ -0,0 +1,143 @@
+# Texas Hold’em poker
+import random
+
+
+class Card:
+    """Represents a standard playing card.
+
+    Attributes:
+      suit: integer 0-3
+      rank: integer 1-13
+    """
+
+    suit_names = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    rank_names = [None, "Ace", "2", "3", "4", "5", "6", "7",
+                  "8", "9", "10", "Jack", "Queen", "King"]
+
+    def __init__(self, suit=0, rank=2):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        """Returns a human-readable string representation."""
+        return "%s of %s" % (Card.rank_names[self.rank],
+                             Card.suit_names[self.suit])
+
+    def __lt__(self, other):
+        """Compares this card to other, first by suit, then rank.
+
+        returns: boolean
+        """
+        if self.rank < other.rank:
+            return True
+        if self.rank > other.rank:
+            return False
+        return self.suit < other.suit
+
+
+class Deck:
+    """Represents a deck of cards.
+
+    Attributes:
+      cards: list of Card objects.
+    """
+
+    def __init__(self):
+        """Initializes the Deck with 52 cards.
+        """
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                card = Card(suit, rank)
+                self.cards.append(card)
+
+    def __str__(self):
+        """Returns a string representation of the deck.
+        """
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return '\n'.join(res)
+
+    def add_card(self, card):
+        """Adds a card to the deck.
+
+        card: Card
+        """
+        self.cards.append(card)
+
+    def remove_card(self, card):
+        """Removes a card from the deck or raises exception if it is not there.
+
+        card: Card
+        """
+        self.cards.remove(card)
+
+    def pop_card(self, i=-1):
+        """Removes and returns a card from the deck.
+
+        i: index of the card to pop; by default, pops the last card.
+        """
+        return self.cards.pop(i)
+
+    def shuffle(self):
+        """Shuffles the cards in this deck."""
+        random.shuffle(self.cards)
+
+    def sort(self):
+        """Sorts the cards in ascending order."""
+        self.cards.sort()
+
+    def move_cards(self, hand, num):
+        """Moves the given number of cards from the deck into the Hand.
+
+        hand: destination Hand object
+        num: integer number of cards to move
+        """
+        for i in range(num):
+            hand.add_card(self.pop_card())
+
+
+class Hand(Deck):
+    """Represents a hand of playing cards."""
+
+    def __init__(self, label=""):
+        self.cards = []
+        self.label = label
+
+
+class PokerHand(Hand):
+    """Represents a poker hand."""
+
+    def suit_hist(self):
+        """Builds a histogram of the suits that appear in the hand.
+
+        Stores the result in attribute suits.
+        """
+        self.suits = {}
+        for card in self.cards:
+            self.suits[card.suit] = self.suits.get(card.suit, 0) + 1
+
+    def has_flush(self):
+        """Returns True if the hand has a flush, False otherwise.
+
+        Note that this works correctly for hands with more than 5 cards.
+        """
+        self.suit_hist()
+        for val in self.suits.values():
+            if val >= 5:
+                return True
+        return False
+
+
+deck = Deck()
+deck.shuffle()
+
+# deal the cards and classify the hands
+for i in range(10):
+    hand = PokerHand()
+    deck.move_cards(hand, 5)
+    hand.sort()
+    # print(hand)
+    print(hand.has_flush())
+    print("")
