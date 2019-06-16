9eacff0b5514fb7d234f4dd35078f2d886a8e0ad
Michael Huang <50892089+MichaelHuangXiaifei@users.noreply.github.com>
2019-06-16 20:44:16 +0800

    Add files via upload

--- project4.py
+++ project4.py
@@ -110,22 +110,9 @@ class Hand(Deck):
     #     for card in self.cards:
     #         self.suits[card.suit] = self.suits.get(card.suit, 0) + 1
 
-    def has_flush(self):
-        """Returns True if the hand has a flush, False otherwise.
-
-        Note that this works correctly for hands with more than 5 cards.
-        """
-        suit_hist = {}
-        for card in self.cards:
-            suit_hist[card.suit] = suit_hist.get(card.suit, 0) + 1
-        for val in suit_hist.values():
-            if val >= 5:
-                return True
-        return False
-
     def has_pair(self):
         """Returns True if the hand has a pair, False otherwise."""
-        rank_hist = {}
+        rank_hist = dict()
         for card in self.cards:
             rank_hist[card.rank] = rank_hist.get(card.rank, 0) + 1
         for val in rank_hist.values():
@@ -146,9 +133,70 @@ class Hand(Deck):
                 return True
         return False
 
+    def has_three_of_a_kind(self):
+        """Returns True if the hand has a pair, False otherwise."""
+        rank_hist = dict()
+        for card in self.cards:
+            rank_hist[card.rank] = rank_hist.get(card.rank, 0) + 1
+        for val in rank_hist.values():
+            if val == 3:
+                return True
+        return False
+
+    def has_straight(self):
+        rank_hist = dict()
+        for card in self.cards:
+            rank_hist[card.suit] = rank_hist.get(card.rank, 0) + 1
+        if rank_hist.get(13, 0) >= 1:
+            if rank_hist.get(1, 0) >= 1:
+                rank_hist[14] = rank_hist[1]
+                del rank_hist[1]
+        rank_list = list()
+        for rank in rank_hist:
+            for time in range(rank_hist[rank]):
+                rank_list.append(rank)
+        rank_list.shuffle()
+
+
+    def has_flush(self):
+        """Returns True if the hand has a flush, False otherwise.
+
+        Note that this works correctly for hands with more than 5 cards.
+        """
+        suit_hist = dict()
+        for card in self.cards:
+            suit_hist[card.suit] = suit_hist.get(card.suit, 0) + 1
+        for val in suit_hist.values():
+            if val >= 5:
+                return True
+        return False
+
+    def has_full_house(self):
+        """Returns True if the hand has full house, False otherwise."""
+        if self.has_three_of_a_kind():
+            rank_hist = dict()
+            for card in self.cards:
+                rank_hist[card.rank] = rank_hist.get(card.rank, 0) + 1
+            for number in rank_hist.values():
+                if number == 2:
+                    return True
+        return False
+
+    def has_four_of_a_kind(self):
+        """Returns True if the hand has four of a kind, False otherwise."""
+        rank_hist = dict()
+        for card in self.cards:
+            rank_hist[card.rank] = rank_hist.get(card.rank, 0) + 1
+        for val in rank_hist.values():
+            if val == 4:
+                return True
+        return False
+
+    def has_straight_flush(self):
+        pass
+
     # def __str__(self):
     #     for card in self.cards:
-
 
     def __init__(self, label=""):
         self.cards = []
